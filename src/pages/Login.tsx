76b11031cbca9d5d3167110c6f09da9a81abab21
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-08-09 19:45:07 -0300

    requisito 1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,70 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { enviarEmail } from '../redux/actions';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  isEmailValid: false,
+  isPassWordValid: false,
+};
+
 function Login() {
-  return <div>Login</div>;
+  const [data, setData] = useState(INITIAL_STATE);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const isEmailValid = (email: string) => email.includes('@') && email.includes('.com');
+  const isPasswordValid = (password: string) => password.length >= 6;
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setData((prevData) => ({
+      ...prevData,
+      email: value,
+      isEmailValid: isEmailValid(value),
+    }));
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setData((prevData) => ({
+      ...prevData,
+      password: value,
+      isPassWordValid: isPasswordValid(value),
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(enviarEmail(data.email, data.password));
+    navigate('/carteira');
+  };
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        data-testid="email-input"
+        onChange={ handleEmailChange }
+        value={ data.email }
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        data-testid="password-input"
+        onChange={ handlePasswordChange }
+        value={ data.password }
+      />
+      <button type="submit" disabled={ !(data.isEmailValid && data.isPassWordValid) }>
+        Entrar
+      </button>
+    </form>
+  );
 }
 
 export default Login;
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+
+export const enviarEmail = (email: string, password: string) => ({
+  type: 'SAVE_EMAIL',
+  payload: { email, password },
+});
